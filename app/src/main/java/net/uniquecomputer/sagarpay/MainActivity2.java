ea0dd3d9c800991120c21abaa58e747140e273fe
peaceuniquesagar <peaceunique2003@gmail.com>
2021-09-23 10:13:06 +0530

    2 Commit

--- app/src/main/java/net/uniquecomputer/sagarpay/MainActivity2.java
+++ app/src/main/java/net/uniquecomputer/sagarpay/MainActivity2.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
@@ -16,12 +17,26 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity2.this, MainActivity3.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+
         nameText = findViewById(R.id.Mname);
         numberText = findViewById(R.id.Mnumber);
 
         Intent i = getIntent();
         name = i.getStringExtra(NAME);
         number = i.getIntExtra(NUMBER,0);
+
+        nameText.setText("Name: "+name);
+        numberText.setText("your transaction ID "+number);
     }
 }
 
